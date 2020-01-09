2d828ed176eed02ebe69f2910a487f487569a266
RamilHinshaw <RamilHinshaw@gmail.com>
2020-01-09 17:40:53 -0600

    updated shaders for gles3

---
+++ assets/shaders/basicShaderES.fs
@@ -0,0 +1,21 @@
+#version 300 es
+precision mediump float;
+
+out vec4 FragColor;
+
+in vec4 outColor;
+in vec2 outTexCoord;
+
+uniform sampler2D ourTexture;
+
+void main()
+{
+    FragColor = outColor;
+    // vec4 texColor = outColor * texture(ourTexture, outTexCoord);
+
+    // //Tranparency
+    // if (texColor.a < 0.1)
+    //     discard;
+
+    // FragColor = texColor;
+}
