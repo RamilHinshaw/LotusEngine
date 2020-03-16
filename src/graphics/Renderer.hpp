7abae1bea8555474ffc646297489bfda2ec2b8cd
RamilHinshaw <RamilHinshaw@gmail.com>
2020-03-15 22:50:03 -0500

    update renderer

--- src/graphics/Renderer.hpp
+++ src/graphics/Renderer.hpp
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <vector>
+
 class Renderer
 {
     public:
+        void Render(); //Render All Verticies to buffer
+        void Sort(); //Sort State Changes
+        void Flush(); //Clean Buffer
+
     private:
+        // List of Meshes
+        // List of Shaders
+        // List of Textures
 }
