67f182f7a98da946949a30fc1ab594f8bef463ab
RamilHinshaw <RamilHinshaw@gmail.com>
2020-04-28 02:50:56 -0500

    graphics logic class

---
+++ src/core/Graphics.cpp
@@ -0,0 +1,28 @@
+#include "Graphics.hpp"
+
+#include <iostream>
+
+void Graphics::DrawQuad()
+{
+
+}
+
+void Graphics::DrawGrid()
+{
+    
+}
+
+void Graphics::DrawCube()
+{
+    
+}
+
+void Graphics::DrawSphere()
+{
+    
+}
+
+void Graphics::DrawText()
+{
+    
+}
